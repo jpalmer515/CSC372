c3c728e04b6c84d436bcc6b5c63bb572672cb6a7
jakepalmer <jpalmer515@gmail.com>
2024-06-09 10:25:12 -0600

    Initial setup of CheckingAccount

--- CSC372_CTA1/BankAccount.java
+++ CSC372_CTA1/BankAccount.java
@@ -1,56 +1,54 @@
 package CSC372_CTA1;
 //Setup constructor with variables
 //Need a list to hold transaction information
+//Use list to hold customer information, that will validate their balance and transaction history
+
 import java.util.Scanner;
 import java.util.ArrayList;
 public class BankAccount {
     Scanner userInput = new Scanner(System.in);
 
-    ArrayList transactionHistory = new ArrayList();
+    ArrayList accountInformation = new ArrayList();
     
     private String firstName;
     private String lastName;
     private int accountID;
     private double balance;
-    private double depositAmount;
-    private double withdrawalAmount;
-    private double updatedBalance;
 
-    //Person object that will return the following information each time it's actioned
+    //Person object
     public void customerInfo() {
         try {
             this.firstName = firstName;
             this.lastName = lastName;
             this.accountID = accountID;
-            this.balance = balance;
         }
         catch (Exception e) {
             System.out.println("Error!" + e.getMessage());
         }
     }
 
-        public void deposit() {
-            System.out.println("Please enter the amount you'd like to deposit: ");
-            double depositAmount = userInput.nextDouble(); 
-            updatedBalance = balance + depositAmount;
-            System.out.printf("Your new balance: $" + updatedBalance);
-        }
+    public void deposit() {
+        System.out.println("Please enter the amount you'd like to deposit: ");
+        double depositAmount = userInput.nextDouble(); 
+        balance = balance + depositAmount;
+        System.out.printf("Your new balance: $" + balance);
+    }
 
-        public void withdrawal() {
-            System.out.println("Please enter the amount you'd like to withdraw: ");
-            double withdrawalAmount = userInput.nextDouble();
-            updatedBalance = balance - withdrawalAmount;
-            System.out.printf("Your new balance: $" + updatedBalance);
-        }
+    public void withdrawal() {
+        System.out.println("Please enter the amount you'd like to withdraw: ");
+        double withdrawalAmount = userInput.nextDouble();
+        balance = balance - withdrawalAmount;
+        System.out.printf("Your new balance: $" + balance);
+    }
 
-        public void getBalance() {
-            System.out.printf("Your balance is: $" + balance);
-        }
+    public void getBalance() {
+        System.out.printf("Your balance is: $" + balance);
+    }
 
-        public void accountSummary() {
-            System.out.printf("Your balance is: $" + balance);
-            System.out.println("Below is your account transaction history: ");
-            System.out.println("-------------------------------------------");
-            System.out.println(transactionHistory);
-        }
+    public void accountSummary() {
+        System.out.printf("Your balance is: $" + balance);
+        System.out.println("Below is your account transaction history: ");
+        System.out.println("-------------------------------------------");
+        System.out.println(accountInformation);
+    }
 }
--- CSC372_CTA1/CheckingAccount.java
+++ CSC372_CTA1/CheckingAccount.java
@@ -1,5 +1,16 @@
 package CSC372_CTA1;
 
+//Apply interest rate (when? what triggers the interest rate?)
+//Every time the withdrawal method is done, have it run the overdraftFee method
+//Check against a balance < 0.00
+
 public class CheckingAccount {
     
+    public void accountInterest() {
+
+    }
+
+    public void overdraftFee() {
+
+    }
 }
