8e4684e133785e481f7b9292f55535a32065dc69
jakepalmer <jpalmer515@gmail.com>
2024-06-16 15:48:52 -0600

    Co-authored-by: kjd12345 <kjd12345@users.noreply.github.com>

--- BankAccountGUI.java
+++ BankAccountGUI.java
@@ -9,19 +9,19 @@ public class BankAccountGUI {
 
     static JButton buttonOne, buttonTwo, buttonThree, buttonFour;
 
-    static JLabel label;
+    static JLabel labelBalance, labelDeposit, labelWithdraw;
 
     static JTextField fieldBalance, fieldDeposit, fieldWithdraw;
-
-    // private void initialize() {
-
-    // }
 
     public static void main(String[] args) {
 
         //Initializing a frame with a title and a label for the menu
         frame = new JFrame("Bank Account");
-        label = new JLabel("Bank Balance");
+
+
+        labelBalance = new JLabel("Bank Balance");
+        labelDeposit = new JLabel("Deposit Amount");
+        labelWithdraw = new JLabel("Withdraw Amount");
 
         //Initializing buttons to display the account balance, deposit or withdraw money
         buttonOne = new JButton("Balance");
@@ -33,30 +33,42 @@ public class BankAccountGUI {
         fieldBalance = new JTextField(20);
         fieldDeposit = new JTextField(20);
         fieldWithdraw = new JTextField(20);
-
+        
         //Initializing the panel
         JPanel panelOne = new JPanel();
-
+        
+        
         //Adding the button to the new panel
         panelOne.add(buttonOne);
         panelOne.add(buttonTwo);
         panelOne.add(buttonThree);
         panelOne.add(buttonFour);
 
-        //Adding text fields
+        //Adding labels with text fields
+        panelOne.add(labelBalance);
         panelOne.add(fieldBalance);
+
+        panelOne.add(labelDeposit);
         panelOne.add(fieldDeposit);
+        
+        panelOne.add(labelWithdraw);
         panelOne.add(fieldWithdraw);
 
         //Setting the background of the panel
         panelOne.setBackground(Color.lightGray);
 
         //Setting label color and size
-        label.setForeground(Color.CYAN);
-        label.setSize(50, 50);
+        labelBalance.setForeground(Color.BLUE);
+        labelBalance.setSize(50, 50);
+
+        labelDeposit.setForeground(Color.BLUE);
+        labelDeposit.setSize(50, 50);
+
+        labelWithdraw.setForeground(Color.BLUE);
+        labelWithdraw.setSize(50, 50);
 
         //Setting the frame size
-        frame.setSize(500, 500);
+        frame.setSize(400, 250);
 
         //Setting frame location
         frame.setLocationRelativeTo(null);
@@ -67,7 +79,41 @@ public class BankAccountGUI {
         //Adding the panel to the frame
         frame.add(panelOne, BorderLayout.CENTER);
 
-        //Exit Action
+        //Instantiating initial bank account balance
+        BankAccount grabAccount = new BankAccount();
+        String accountBalance = Double.toString(grabAccount.getBalance());
+        fieldBalance.setText(accountBalance);
+
+        //Display balance action
+        buttonOne.addActionListener(new ActionListener() {
+            
+            public void actionPerformed(ActionEvent e) {
+                String accountBalance = Double.toString(grabAccount.getBalance());
+                fieldBalance.setText(accountBalance);
+            }
+        });
+
+        //User deposit action
+        buttonTwo.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String userDeposit = fieldDeposit.getText();
+                grabAccount.deposit(Double.parseDouble(userDeposit));
+                fieldBalance.setText(Double.toString(grabAccount.getBalance()));
+                fieldDeposit.setText("");
+            }
+        });
+
+        //User withdraw action
+        buttonThree.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String userWithdraw = fieldWithdraw.getText();
+                grabAccount.withdrawal(Double.parseDouble(userWithdraw));
+                fieldBalance.setText(Double.toString(grabAccount.getBalance()));
+                fieldWithdraw.setText("");
+            }
+        });
+        
+        // Exit Action
         buttonFour.addActionListener(new ActionListener() {
             
             public void actionPerformed(ActionEvent e) {
@@ -75,6 +121,5 @@ public class BankAccountGUI {
                 frame.dispose();
             }
         });
-
     }
 }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,7 @@
+        // buttonOne.addActionListener(new ActionListener() {
+            
+        //     public void actionPerformed(ActionEvent e) {
+        //         BankAccount accountBalance = new BankAccount();
+        //         fieldBalance.append(accountBalance);
+        //     }
+        // });
