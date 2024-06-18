bfbb5ea633710cbadda105871855fa435d5f6e65
jakepalmer <jpalmer515@gmail.com>
2024-06-18 09:59:02 -0600

    Added JavaFX jars to CSC372

--- BankUserInterface.java
+++ BankUserInterface.java
@@ -1,23 +1,11 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.stage.Stage;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-
-
-import java.awt.Color;
-import java.awt.Menu;
-import java.awt.MenuBar;
-import java.awt.MenuComponent;
-import java.awt.MenuItem;
-import java.awt.event.ActionEvent;
-import java.beans.EventHandler;
-import javax.swing.*;
 
 
 public class BankUserInterface extends Application {
@@ -26,8 +14,6 @@ public class BankUserInterface extends Application {
     public void start(Stage newBankInterface) throws Exception{
 
         newBankInterface.setTitle("Bank Menu");
-
-        Menu bankMenu = Menu("Menu");
 
         //Setting what the menu items display
         MenuItem dateTimeDisplay = new MenuItem("Date/Time");
@@ -39,13 +25,7 @@ public class BankUserInterface extends Application {
         MenuButton bankMenuButton = new MenuButton("Bank Menu", dateTimeDisplay, logTextContents, randomHueGreen, exitProgram);
 
         //Creating a vertical menu button with user options
-        Vbox verticalBankMenu = new Vbox(bankMenuButton);
-
-        //Create a menubar
-        Menubar bankMenuBar = new Menubar();
-
-        //Add menu to menubar
-        bankMenuBar.getMenus().add(bankMenu);
+        VBox verticalBankMenu = new VBox(bankMenuButton);
 
         //Create a new scene
         Scene bankInterfaceScene = new Scene(400, 400, Color.GREEN);
