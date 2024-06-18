b480c7a002f093e2225984b5a910f4541a8741b7
jakepalmer <jpalmer515@gmail.com>
2024-06-18 11:25:39 -0600

    Setting actions

--- BankUserInterface.java
+++ BankUserInterface.java
@@ -1,3 +1,8 @@
+import java.util.*;
+import java.awt.TextField;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -6,7 +11,7 @@ import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
+import javafx.scene.paint.*;
 
 public class BankUserInterface extends Application {
 
@@ -22,18 +27,44 @@ public class BankUserInterface extends Application {
         MenuItem exitProgram = new MenuItem("Exit");
 
         //Creating menu buttons
-        MenuButton bankMenuButton = new MenuButton("Bank Menu", dateTimeDisplay, logTextContents, randomHueGreen, exitProgram);
+        MenuButton bankMenuButton = new MenuButton("Bank Menu", null, dateTimeDisplay, logTextContents, randomHueGreen, exitProgram);
 
         //Creating a vertical menu button with user options
         VBox verticalBankMenu = new VBox(bankMenuButton);
 
+
+        //Creating text field
+        Textfield emptyFieldOne = new TextField();
+
         //Create a new scene
-        Scene bankInterfaceScene = new Scene(400, 400, Color.GREEN);
+        Scene bankInterfaceScene = new Scene(verticalBankMenu, 200, 100);
+        bankInterfaceScene.setFill(Color.GREEN);
         newBankInterface.setScene(bankInterfaceScene);
         newBankInterface.show();
 
-        dateTimeDisplay.setOnAction((EventHandlerActionEvent)new EventHandlerActionEvent() {
+        //Retrieving and displaying the date and time
+        dateTimeDisplay.setOnAction(new EventHandler<ActionEvent>() {
 
+            public void handle(ActionEvent event) {
+                String currentDateTime = new SimpleDateFormat("yyyy-mmm-dd HH:mm").format(new Date());
+                emptyFieldOne.setText("Date/Time: " + currentDateTime);
+            }
+        });
+
+        //Convert text box string into a "log.txt" file
+        logTextContents.setOnAction(new EventHandler<ActionEvent>() {
+            
+            public void handle(ActionEvent event) {
+                String logFilePath = "C:\\Users\\Jake\\OneDrive\\Desktop\\Folder\\School\\CSU Global\\CSC372\\CSC372_CTA3.txt";
+                BufferedWriter logWriter = new BufferedWriter(new FileWriter(logFilePath)) {
+                    logWriter.write(emptyFieldOne);
+                }
+            }
+        });
+
+        //Change background color to different hue of green
+        randomHueGreen.setOnAction(new EventHandler<ActionEvent>() {
+            
             public void handle(ActionEvent event) {
 
             }
