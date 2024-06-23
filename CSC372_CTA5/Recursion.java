57300a865ab000b5bf10e747a69ce7146106af90
jakepalmer <jpalmer515@gmail.com>
2024-06-23 13:45:17 -0600

    First attempt at CTA5

--- CSC372_CTA5/Recursion.java
+++ CSC372_CTA5/Recursion.java
@@ -1,5 +1,20 @@
-
+import java.util.Scanner;
 
 public class Recursion {
+
+    //User input for the integers
+    int recursionNumber = UserInput.nextInt();
+
+    //Recursive method, multiplying use integers
+    public int product(int n) {
     
+    System.out.println("Please enter a whole number: ");
+    
+    if (n < 6) {
+        return product(n * recursionNumber);
+    }
+    else
+        return n;
+        System.out.println(n);
+    }
 }
