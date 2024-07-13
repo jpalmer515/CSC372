069fe4e6baa5988d87c4f1692511a3554e3c7d3f
jakepalmer <jpalmer515@gmail.com>
2024-07-13 13:48:17 -0600

    Start of StudenDriver sorting algorithm, passing in the Comparator classes

--- CSC372_CTA6/Student.java
+++ CSC372_CTA6/Student.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Comparator;
 
 public class Student {
@@ -13,32 +12,8 @@ public class Student {
         this.address = address;
     }
 
-    public static void main(String[] args) {
-
-        ArrayList<Student> StudentList = new ArrayList<>();
-
-        Student student1 = new Student("Harrison Martin", 100, "2717 Hill Street");
-        Student student2 = new Student("Evangeline Park", 101, "4344 Dark Hollow Road");
-        Student student3 = new Student("Brenda Dale", 102, "4061 Cottonwood Lane");
-        Student student4 = new Student("Santiago Beltran", 103, "634 Gladwell Street");
-        Student student5 = new Student("Nathan Middleton", 104, "3727 Mount Street");
-        Student student6 = new Student("Mabel Mosley", 105, "4571 Ryder Avenue");
-        Student student7 = new Student("Amelie Evans", 106, "4211 Terra Cotta Street");
-        Student student8 = new Student("Leanne Crane", 107, "2125 Walnut Avenue");
-        Student student9 = new Student("Ruby Payne", 108, "3175 Poplar Street");
-        Student student10 = new Student("Sandra Santana", 109, "48 Redbud Drive");
-
-        StudentList.add(student1);
-        StudentList.add(student2);
-        StudentList.add(student3);
-        StudentList.add(student4);
-        StudentList.add(student5);
-        StudentList.add(student6);
-        StudentList.add(student7);
-        StudentList.add(student8);
-        StudentList.add(student9);
-        StudentList.add(student10);
-
+    public String toString() {
+        return "Student{" + name + ", " + rollno + ", " + address + "}";
     }
 }
 
@@ -58,6 +33,3 @@ class CompareStudentRollNo implements Comparator<Student> {
     }
 }
 
-// type variableName;
-// type variableName = initial value;
-// type variableName = newType.converstionfunction();
---
+++ CSC372_CTA6/StudentDriver.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+// import java.util.Arrays;
+
+public class StudentDriver {
+    
+    ArrayList<Student> StudentList = new ArrayList<>();
+
+    void StudentSortingAlgorithm() {
+
+        int StudentListLength = StudentList.length;
+        
+    }
+
+    public void main(String[] args) {
+
+        Student student1 = new Student("Harrison Martin", 16981, "2717 Hill Street");
+        Student student2 = new Student("Evangeline Park", 40735, "4344 Dark Hollow Road");
+        Student student3 = new Student("Brenda Dale", 924454, "4061 Cottonwood Lane");
+        Student student4 = new Student("Santiago Beltran", 21108, "634 Gladwell Street");
+        Student student5 = new Student("Nathan Middleton", 26240, "3727 Mount Street");
+        Student student6 = new Student("Mabel Mosley", 92260, "4571 Ryder Avenue");
+        Student student7 = new Student("Amelie Evans", 01621, "4211 Terra Cotta Street");
+        Student student8 = new Student("Leanne Crane", 86676, "2125 Walnut Avenue");
+        Student student9 = new Student("Ruby Payne", 63541, "3175 Poplar Street");
+        Student student10 = new Student("Sandra Santana", 63682, "48 Redbud Drive");
+
+        StudentList.add(student1);
+        StudentList.add(student2);
+        StudentList.add(student3);
+        StudentList.add(student4);
+        StudentList.add(student5);
+        StudentList.add(student6);
+        StudentList.add(student7);
+        StudentList.add(student8);
+        StudentList.add(student9);
+        StudentList.add(student10);
+
+        
+    }
+}
