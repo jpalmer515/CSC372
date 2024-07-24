846a1bfd17d580f5b60a6cdf9ff5656614a7a278
jakepalmer <jpalmer515@gmail.com>
2024-07-24 17:05:47 -0600

    Started CTA8

--- CSC372_CTA8/StudentData.java
+++ CSC372_CTA8/StudentData.java
@@ -2,4 +2,17 @@ package CSC372_CTA8;
 
 public class StudentData {
     
+    String name;
+    String address;
+    double gpa;
+
+    StudentData(String name, String address, double gpa) {
+        this.name = name;
+        this.address = address;
+        this.gpa = gpa;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
