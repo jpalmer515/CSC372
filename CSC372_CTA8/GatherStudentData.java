51aa3dd990561026c9d6d533317155ac3bbfa603
jakepalmer <jpalmer515@gmail.com>
2024-07-30 20:55:57 -0600

    Co-authored-by: kjd12345 <kjd12345@users.noreply.github.com>

--- CSC372_CTA8/GatherStudentData.java
+++ CSC372_CTA8/GatherStudentData.java
@@ -7,8 +7,28 @@ public class GatherStudentData {
     Scanner StudentDataInput = new Scanner(System.in);
     LinkedList<StudentData> ListedStudentData = new LinkedList<StudentData>();
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
 
+        Scanner StudentDataInput = new Scanner(System.in);
+
+        LinkedList<StudentData> ListedStudentData = new LinkedList<StudentData>();
+        
+        String name;
+        String address;
+        Double gpa;
+
+        StudentData newStudent1 = new StudentData(name, address, gpa);
+
+        System.out.println("Please enter a FIRST and LAST name");
+        name = newStudent1.setName(StudentDataInput.nextLine());
+        
+        System.out.println("Please enter an address");
+        address = newStudent1.setAddress(StudentDataInput.nextLine());
+        
+        System.out.println("Please enter a GPA in the format of X.X, where X is a whole number");
+        gpa = newStudent1.setGpa(StudentDataInput.nextDouble());
+
+        System.out.println(newStudent1);
         //for loop here
 
         System.out.println();
@@ -19,5 +39,5 @@ public class GatherStudentData {
 //create a linked list to store the student data
 //create a for loop to prompt a user to enter the appropriate data
 //create a checker to validate the numeric data being entered for gpa
-//ouput linked list in ascending order by name, could probably use the collections library
+//output linked list in ascending order by name, could probably use the collections library
 //output to text file
--- CSC372_CTA8/StudentData.java
+++ CSC372_CTA8/StudentData.java
@@ -14,32 +14,42 @@ public class StudentData {
         this.gpa = gpa;
     }
 
+    public void setStudentData(String name, String address, double gpa) {
+        this.name = name;
+        this.address = address;
+        this.gpa = gpa;
+    }
+
+    //Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setGpa(Double gpa) {
+        this.gpa = gpa;
+    }
+    
+    //Getters
+    public String getName(String name) {
+    return this.name;
+    }
+    public String getAddress(String address) {
+        return this.address;
+    }
+    public Double getGpa(Double gpa) {
+        return this.gpa;
+    }
+
     public String toString() {
         return name + ", " + address + ", " + gpa;
     }
 
-    public void main(String[] args) {
-
-    Scanner StudentDataInput = new Scanner(System.in);
-
-    LinkedList<StudentData> ListedStudentData = new LinkedList<StudentData>();
-
-            System.out.println("Please enter a FIRST and LAST name");
-            name = StudentDataInput.nextLine();
-
-            System.out.println("Please enter an address");
-            address = StudentDataInput.nextLine();
-
-            System.out.println("Please enter a GPA in the format of X.X, where X is a whole number");
-            gpa = StudentDataInput.nextDouble();
-
-            StudentData newStudent1 = new StudentData(name, address, gpa);
-
-            System.out.println(newStudent1);
     //for loop will have to create a new studentdata object and then add the parameters to the object from the user input
     //can the for loop iterate and add to an index for the object? student1(0), student1(1)
     //how does a for loop create a new student object iterating through numbers? student1, student2, etc
-    }
+
     //create main method and for loop in this class. 
     //private variables can only be accessed by this class, thus adding the for loop here should allow them to be accessed
 
